475d60763bbfb461b2b3050ab6ea032d5f814f35
Relati <1048646128@qq.com>
2017-06-06 21:32:08 +0900

    完成日记类的数据结构的基本设计以及其基本实现

---
+++ app/src/main/java/experiment/diary/Diary.java
@@ -0,0 +1,35 @@
+package com.study.android.kehan.diary_test01;
+
+public class Diary {
+    private int month;
+    private int day;
+    private String title;
+    private String content;
+    private byte[] picture;
+
+    Diary(int month, int day, String title, String content, byte[] picture) {
+        this.month   = month;
+        this.day     = day;
+        this.title   = title;
+        this.content = content;
+        this.picture = picture;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+    public int getDay() {
+        return day;
+    }
+    public int getMonth() {
+        return month;
+    }
+    public String getContent() {
+        return content;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    // TODO: the method of picture changing
+}
