b70ce09ed562b2015427602ce600227f691d9ef3
djy <jeyoteng@gmail.com>
2017-06-09 22:29:15 +0800

    add database adapter, diary crud operation

--- app/src/main/java/experiment/diary/DatabaseAdapter.java
+++ app/src/main/java/experiment/diary/DatabaseAdapter.java
@@ -170,6 +170,166 @@ class DatabaseAdapter {
     }
 
 
+    // TODO: insert picture with suitable type
+    boolean insertDiary(int month, int day, String title, String content) {
+        SQLiteDatabase db = dh.getWritableDatabase();
+        if (db.isOpen()) {
+            ContentValues cv = new ContentValues();
+            cv.put("month", month);
+            cv.put("day", day);
+            cv.put("title", title);
+            cv.put("content", content);
+            // -1 means insert failed
+            if (db.insert(APP_DIARY_DB_DIARIES_TABLE, null, cv) != -1) {
+                db.close();
+                return true;
+            }
+            db.close();
+        }
+        // if code executed here then error in insert and handled it by SQLiteDatabase
+        Log.e(TAG, "insertDiary: cannot open db", new Throwable("cannot open db"));
+        return false;
+    }
+    // TODO: update picture with suitable type
+    boolean updateDiary(int month, int day, String title, String content) {
+        SQLiteDatabase db = dh.getWritableDatabase();
+        if (db.isOpen()) {
+            ContentValues cv = new ContentValues();
+            cv.put("month", month);
+            cv.put("day", day);
+            cv.put("title", title);
+            cv.put("content", content);
 
+            String where = "month = ? AND day = ?";
+            String whereArgs[] = {String.valueOf(month), String.valueOf(day)};
+
+            // db.update return the rows affected, so if succeeded return value greater then 0
+            if (db.update(APP_DIARY_DB_USER_TABLE, cv, where, whereArgs) > 0) {
+                db.close();
+                return true;
+            }
+            db.close();
+        }
+        // if code executed here then error in insert and handled it by SQLiteDatabase
+        Log.e(TAG, "updateDiary: cannot open db", new Throwable("cannot open db"));
+        return false;
+    }
+    boolean deleteDiary(int month, int day) {
+        SQLiteDatabase db = dh.getWritableDatabase();
+        if (db.isOpen()) {
+
+            String where = "month = ? AND day = ?";
+            String whereArg[] = {String.valueOf(month), String.valueOf(day)};
+
+            // db.delete return the rows affected, so if succeeded return value greater then 0
+            if (db.delete(APP_DIARY_DB_DIARIES_TABLE, where, whereArg) > 0) {
+                db.close();
+                return true;
+            }
+            db.close();
+        }
+        Log.e(TAG, "deleteDiary: cannot open db", new Throwable("cannot open db"));
+        return false;
+    }
+
+    ArrayList<ArrayList<Diary>> queryDiaryAll() {
+        ArrayList<ArrayList<Diary>> result = new ArrayList<>();
+        for (int i = 0; i <= 12; ++i) {
+            result.add(new ArrayList<Diary>());
+        }
+        SQLiteDatabase db = dh.getReadableDatabase();
+
+        if (db.isOpen()) {
+            // TODO: picture
+            String columns[] = {"month", "day", "title", "content"};
+            Cursor cursor = db.query(APP_DIARY_DB_DIARIES_TABLE, columns, null, null, null, null, null);
+
+            // convert the cursor into Diary and add it into result
+            if (cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()) {
+                do {
+                    int month = cursor.getInt(cursor.getColumnIndex("month"));
+                    int day = cursor.getInt(cursor.getColumnIndex("day"));
+                    String title = cursor.getString(cursor.getColumnIndex("title"));
+                    String content = cursor.getString(cursor.getColumnIndex("content"));
+
+                    // TODO: picture
+                    result.get(month).add(new Diary(month, day, title, content, null));
+
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+            db.close();
+            return result;
+        } else {
+            Log.e(TAG, "queryAll: cannot open db", new Throwable("cannot open db"));
+        }
+        return null;
+    }
+    ArrayList<Diary> queryDiaryByMonth(int queryMonth) {
+        ArrayList<Diary> result = new ArrayList<>();
+        SQLiteDatabase db = dh.getReadableDatabase();
+
+        if (db.isOpen()) {
+            // TODO: picture
+            String columns[] = {"month", "day", "title", "content"};
+            String where = "month = ?";
+            String whereArgs[] = {String.valueOf(queryMonth)};
+
+            Cursor cursor = db.query(APP_DIARY_DB_DIARIES_TABLE, columns, where, whereArgs, null, null, null);
+
+            // convert the cursor into Diary and add it into result
+            if (cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()) {
+                do {
+                    int month = cursor.getInt(cursor.getColumnIndex("month"));
+                    int day = cursor.getInt(cursor.getColumnIndex("day"));
+                    String title = cursor.getString(cursor.getColumnIndex("title"));
+                    String content = cursor.getString(cursor.getColumnIndex("content"));
+
+                    // TODO: picture
+                    result.add(new Diary(month, day, title, content, null));
+
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+            db.close();
+            return result;
+        } else {
+            Log.e(TAG, "queryAll: cannot open db", new Throwable("cannot open db"));
+        }
+        return null;
+    }
+    Diary queryDiaryByMonthAndByDay(int queryMonth, int queryDay) {
+        Diary result = null;
+        SQLiteDatabase db = dh.getReadableDatabase();
+        if (db.isOpen()) {
+            // TODO: picture
+            String columns[] = {"month", "day", "title", "content"};
+            String where = "month = ? AND day = ?";
+            String whereArgs[] = {String.valueOf(queryMonth), String.valueOf(queryDay)};
+
+            Cursor cursor = db.query(APP_DIARY_DB_DIARIES_TABLE, columns, where, whereArgs, null, null, null);
+
+            // convert the cursor into Diary and add it into result
+            if (cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()) {
+                do {
+                    int month = cursor.getInt(cursor.getColumnIndex("month"));
+                    int day = cursor.getInt(cursor.getColumnIndex("day"));
+                    String title = cursor.getString(cursor.getColumnIndex("title"));
+                    String content = cursor.getString(cursor.getColumnIndex("content"));
+
+                    // TODO: picture
+
+                    result = new Diary(month, day, title, content, null);
+
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+            db.close();
+            return result;
+        } else {
+            Log.e(TAG, "queryAll: cannot open db", new Throwable("cannot open db"));
+        }
+        return null;
+    }
 
 }
